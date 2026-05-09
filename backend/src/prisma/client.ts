ada459ced4bfe621f586a369c767c70129754ebe
ravindhar1108 <ravindharg1108@gmail.com>
2026-05-09 17:21:21 +0530

    backend fix

--- backend/src/prisma/client.ts
+++ backend/src/prisma/client.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import dotenv from "dotenv";
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
-
